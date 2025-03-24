3b73a6a83794832e1556679b3e5562276aebcc65
Depo S. H <141189770+dxdeposh@users.noreply.github.com>
2025-03-24 13:51:29 +0700

    Membuat Aplikasi Dengan Express.Js Pertama Kali

---
+++ first-express/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use(() => {
+  console.log("we got request");
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on port http://localhost:8080");
+});
