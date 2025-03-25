ac90039e686f27a4a07cc0b7571142292b51cacc
Depo S. H <141189770+dxdeposh@users.noreply.github.com>
2025-03-25 11:44:30 +0700

    Membuat Route Untuk Menentukan Response Tertentu

--- first-express/index.js
+++ first-express/index.js
@@ -2,9 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  console.log("we got request");
-  res.send(`ini string dari respon`);
+// app.use((req, res) => {
+//   console.log("we got request");
+//   res.send(`ini string dari respon`);
+// });
+
+app.get("/", (req, res) => {
+  res.send("this is home page");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("this is a cat page");
+});
+
+app.post("/cats", (req, res) => {
+  res.send("this is a cat page from post");
+});
+
+app.get("/about", (req, res) => {
+  res.send("this is about page");
+});
+
+app.get("*", (req, res) => {
+  res.send("halaman tidak ditemukan");
 });
 
 app.listen(8080, () => {
